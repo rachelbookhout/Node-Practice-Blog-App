a7d1acfe9110620c9e09a99fcc8d6345fa0384d8
rachelbookhout <rbookhou@gmail.com>
2015-06-03 20:09:53 -0400

    added settings

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ var path = require('path');
 var http = require ('http');
 
 var app = express();
+app.set('appName', 'hello-world');
+app.set('port', process.env.PORT || 3000);
+app.set('views',path.join(_dirname, 'views'));
+app.set('view engine', 'jade');
