255683d909cf431b2df192bcfb1687e872b7d8c6
rachelbookhout <rbookhou@gmail.com>
2015-06-05 19:49:03 -0400

    debugged

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var http = require ('http');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.set('views',path.join(_dirname, 'views'));
+app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.all('*', function(req,res){
-    res.render('index', {msg: "Welcome to the Practical Node.js"})
+    res.render(
+        'index',
+        {msg: "Welcome to the Practical Node.js"}
 );
 });
 http.createServer(app).listen(app.get('port'), function(){
