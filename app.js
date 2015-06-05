0dccc2a38294d54b250c1faa6b0de61ce6a45ed1
rachelbookhout <rbookhou@gmail.com>
2015-06-04 20:33:16 -0400

    added logic to start server

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ var path = require('path');
 var http = require ('http');
 
 var app = express();
-app.set('appName', 'hello-world');
+
 app.set('port', process.env.PORT || 3000);
 app.set('views',path.join(_dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.all('*', function(req,res){
+    res.render('index', {msg: "Welcome to the Practical Node.js"})
+);
+});
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express.js server listening on port' + app.get('port'));
+}
+);
