8cf9742efe048015f5c10e6d2f92c635576677a3
rachelbookhout <rbookhou@gmail.com>
2015-06-07 17:13:37 -0400

    trying it out with expect

--- test-example/test.js
+++ test-example/test.js
@@ -1,6 +1,20 @@
-var assert = require ('assert');
-describe('String#split', function(){
-  it ('shoud return an array', function(){
-    assert(Array.isArray('a,b,c'.split(',')));
-  });
+var expect = require('expect.js');
+var expected, current;
+before(function(){
+  expected = ['a','b','c'];
 })
+describe('String#split', function(){
+  beforeEach(function(){
+    current = 'a,b,c'.split(',');
+  })
+  it('should return an array', function(){
+    expect(Array.isArray(current).to.be.true;
+  });
+it ('should return the same array', function(){
+  expect(expected.length).to.equal(current.length);
+  for (var i=0; i<expected.length; i++){
+    expect(expected[i].equal(current[i]);
+  }
+  })
+})
+
